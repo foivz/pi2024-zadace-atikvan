7e819bb8226952b0eb1283437dc4412c6b90d3c4
Ada Tikvan <atikvan22@foi.hr>
2024-05-28 19:46:33 +0200

    Implementira prijavu studenta u sustav.

---
+++ Software/StudentskiRecenziraj/StudentskiRecenziraj/FrmLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentskiRecenziraj
+{
+    public partial class FrmLogin : Form
+
+    {
+        string username = "student";
+        string password = "menza";
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!");
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!");
+            }
+            else
+            {
+                if (txtUsername.Text == username && txtPassword.Text == password)
+                {
+                    FrmRecenzije frmRecenzije = new FrmRecenzije();
+                    Hide();
+                    frmRecenzije.ShowDialog();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Krivi korisnički podaci!");
+                }
+
+            }
+        }
+    }
+}
---
+++ Software/StudentskiRecenziraj/StudentskiRecenziraj/Modeli/Recenzija.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentskiRecenziraj.Modeli
+{
+    public class Recenzija
+    {
+        public int ocjenaOkus { get; set; }
+        public string ocjenaKolicina { get; set; }
+        public string komentar { get; set; }
+
+    }
+}
